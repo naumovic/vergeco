2d7bc42cafd55493489812c22ae1c6a7c320736f
Mihajlo Naumovic <mihajlo.naumovic@gmail.com>
2020-10-25 12:18:57 +1000

    Updated footer
    Relvant information added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,14 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://twitter.com/naumovic">@naumovic</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          <a href="https://www.vergeco.com.au/">vergeCo</a>.
         </span>
       </div>
     </footer>
